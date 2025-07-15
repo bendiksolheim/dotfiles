2bfbc5b4c0c050b5980f75f4e9dec58f361a6222
Bendik Solheim <hello@bendik.dev>
2025-07-15 21:33:30 +0200

    Sketchybar: remove unused var

--- .config/sketchybar/plugins/wifi.sh
+++ .config/sketchybar/plugins/wifi.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env sh
 
-CURRENT_WIFI="$(/System/Library/PrivateFrameworks/Apple80211.framework/Versions/Current/Resources/airport -I)"
 SSID="$(ipconfig getsummary en0 | awk -F ' SSID : ' '/ SSID : / {print $2}')"
 
 if [ "$SSID" = "" ]; then
